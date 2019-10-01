57cd32b601cfc2afe84c130305852ad96091cc74
Mashuk, Dmitry <d.mashuk@itransition.com>
2019-10-01 15:26:52 +0300

    Starting configuration the "webpack"

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+let conf = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'main.js',
+		publicPath: 'dist/'
+	},
+	devServer: {
+		overlay: true
+	}
+};
+
+module.exports = () => {
+	return conf;
+};
